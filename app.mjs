86017b02baa74f49b6080bb54130a1b5846fa14d
Michael Girum Kelemework <mg7688@nyu.edu>
2025-11-03 22:23:24 -0500

    Implemented all of the hbs files except the chat page. And also wrote most of the routes in app.mjs

--- app.mjs
+++ app.mjs
@@ -1,27 +1,39 @@
 import './config.mjs';
 import express from 'express'
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
 
 import path from 'path'
 import { fileURLToPath } from 'url';
+import { engine } from 'express-handlebars';
+
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// const mongoURI = process.env.DSN
-// mongoose.connect(mongoURI)
-//   .then(() => console.log('MongoDB connected successfully'))
-//   .catch(err => console.error('MongoDB connection error:', err));
 
-// const PersonSchema = new mongoose.Schema({
-//   name: String,
-//   age: Number
-// });
+app.engine('hbs', engine({ 
+  extname: '.hbs',
+  defaultLayout: 'layout',
+  layoutsDir: path.join(__dirname, 'views')
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
-// const Person = mongoose.model('Person', PersonSchema);
-// const mike = await Person.findOne({ name: 'Michael' });
-// console.log(mike);
+
+const mongoURI = process.env.DSN
+mongoose.connect(mongoURI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const PersonSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const Person = mongoose.model('Person', PersonSchema);
+const mike = await Person.findOne({ name: 'Michael' });
+console.log(mike);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -33,7 +45,7 @@ app.set('view engine', 'hbs');
 
 // Basic route placeholders
 app.get('/', (req, res) => {
-  res.send('Welcome to CipherChat!');
+  res.send(mike.name + ' is ' + mike.age + ' years old.');
 });
 
 // TODO: Add routes for:
@@ -42,6 +54,51 @@ app.get('/', (req, res) => {
 // - /home (GET)
 // - /chat/:username (GET)
 // - /profile (GET, POST)
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  res.redirect(req.body.username + "'s password is " + req.body.password);
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  res.redirect('/home');
+});
+
+app.get('/home', (req, res) => {
+  // TODO: Get actual contacts from database
+  const contacts = [
+    { username: 'Alice', lastMessageTime: '7:43 PM', hasUnread: true },
+    { username: 'Bob', lastMessageTime: '2:18 PM', hasUnread: true },
+    { username: 'Charlie', lastMessageTime: '1:01 PM', hasUnread: true },
+    { username: 'David', lastMessageTime: '2:15 PM', hasUnread: false },
+    { username: 'Eve', lastMessageTime: 'Oct 11', hasUnread: true },
+    { username: 'Frank', lastMessageTime: 'Oct 12', hasUnread: false },
+    { username: 'Grace', lastMessageTime: '2:00 AM', hasUnread: true }
+  ];
+  res.render('home', { contacts });
+});
+
+app.get('/home/search', (req, res) => {
+  // TODO: Implement search functionality
+  res.redirect('/home');
+});
+
+app.get('/chat/:username', (req, res) => {
+  res.send(`Chat with ${req.params.username} - To be implemented`);
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile');
+});
+app.post('/profile', (req, res) => {
+  res.redirect('/profile');
+});
 
 
 
